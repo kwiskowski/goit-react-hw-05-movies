19f82c81b2859ba142c53a70f3808c2788a8712f
Krzysztof Wiskowski <krzysztof.wiskowski@gmail.com>
2024-02-25 12:10:46 +0100

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -24,30 +24,3 @@ export const App = () => {
     </Routes>
   );
 };
-
-// import { Routes, Route } from 'react-router-dom';
-// import { SharedLayout } from './SharedLayout/SharedLayout';
-// import Home from './pages/Home/Home';
-// import MovieDetails from './pages/MovieDetails/MovieDetails';
-// import Movies from './pages/Movies/Movies';
-// import Reviews from './Reviews/Revievs';
-// import Cast from './Cast/Cast';
-// import css from './App.module.css';
-
-// export const App = () => {
-//   return (
-//     <div className={css.appContainer}>
-//       <Routes>
-//         <Route path="/" element={<SharedLayout />}>
-//           <Route index element={<Home />} />
-//           <Route path="/movies" element={<Movies />} />
-//           <Route path="/movies/:movieId" element={<MovieDetails />}>
-//             <Route path="/movies/:movieId/cast" element={<Cast />} />
-//             <Route path="/movies/:movieId/reviews" element={<Reviews />} />
-//           </Route>
-//           <Route path="*" element={<Home />} />
-//         </Route>
-//       </Routes>
-//     </div>
-//   );
-// };
